9126f8483945e01e32c33d2cfd92c6fd0374029c
Pramadha Yaramakala <pramadhayaramakala@Pramadhas-MacBook-Pro.local>
2024-04-25 14:04:13 +0530

    geek

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingScreen from './screens/LandingScreen.js'
+import QuoteScreen from './screens/QuoteScreen.js';
+import RestrauntsScreen from './screens/RestrauntsScreen.js';
+import FoodsScreen from "./screens/FoodsScreen.js"
+import ContactScreen from './screens/ContactScreen.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppTemplate from './components/AppTemplate.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AppTemplate/>}>
+            <Route path='' element= {<LandingScreen/>}/>
+            <Route path='quotes' element={<QuoteScreen/>}/>
+            <Route path='restraunts' element= {<RestrauntsScreen/>} />
+            <Route path='foods' element={<FoodsScreen/>} />
+            <Route path='contact' element={<ContactScreen/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
